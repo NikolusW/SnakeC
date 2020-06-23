61b6726b06c7a1e4424db78592e49b84dceb7318
Nikolus Williams <Nik@ou.edu>
2020-06-22 19:38:08 -0500

    Created initial setup for snake game

---
+++ game/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+bool gameOver;
+const int width = 20;
+const int height = 20;
+void Setup(){
+    gameOver = false;
+
+
+}
+void Draw(){
+
+
+
+}
+void Input(){
+
+
+
+}
+void Logic(){
+
+
+
+}
+int main()
+{
+    Setup();
+    while(!gameOver){
+        Draw();
+        Input();
+        Logic();
+        Sleep(10);
+    }
+    return 0;
+}
